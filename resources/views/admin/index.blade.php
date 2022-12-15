7dcbd0fb4895e69bfa5f6cee8a7840ed1f704414
Yakub Muhammed <yakubmuhammed51@gmail.com>
2022-12-16 00:14:21 +0100

    setup Users dashbord

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,84 @@
+@extends('users.user_template')
+@section('main_contents')
+<div class="sl-mainpanel">
+      <nav class="breadcrumb sl-breadcrumb">
+        <a class="breadcrumb-item" href="index.html">Starlight</a>
+        <a class="breadcrumb-item" href="index.html">Tables</a>
+        <span class="breadcrumb-item active">Data Table</span>
+      </nav>
+
+      <div class="sl-pagebody">
+        <div class="sl-page-title">
+          <h5>Data Table</h5>
+          <p>DataTables is a plug-in for the jQuery Javascript library.</p>
+        </div><!-- sl-page-title -->
+
+        <div class="card pd-20 pd-sm-40">
+          <h6 class="card-body-title">Basic Responsive DataTable</h6>
+          <p class="mg-b-20 mg-sm-b-30">Searching, ordering and paging goodness will be immediately added to the table, as shown in this example.</p>
+
+          <div class="table-wrapper">
+            <table id="datatable1" class="table display responsive nowrap">
+              <thead>
+                <tr>
+                  <th class="wd-15p">First name</th>
+                  <th class="wd-15p">Last name</th>
+                  <th class="wd-20p">Position</th>
+                  <th class="wd-15p">Start date</th>
+                  <th class="wd-10p">Salary</th>
+                  <th class="wd-25p">E-mail</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Tiger</td>
+                  <td>Nixon</td>
+                  <td>System Architect</td>
+                  <td>2011/04/25</td>
+                  <td>$320,800</td>
+                  <td>t.nixon@datatables.net</td>
+                </tr>
+                <tr>
+                  <td>Jonas</td>
+                  <td>Alexander</td>
+                  <td>Developer</td>
+                  <td>2010/07/14</td>
+                  <td>$86,500</td>
+                  <td>j.alexander@datatables.net</td>
+                </tr>
+                <tr>
+                  <td>Shad</td>
+                  <td>Decker</td>
+                  <td>Regional Director</td>
+                  <td>2008/11/13</td>
+                  <td>$183,000</td>
+                  <td>s.decker@datatables.net</td>
+                </tr>
+                <tr>
+                  <td>Michael</td>
+                  <td>Bruce</td>
+                  <td>Javascript Developer</td>
+                  <td>2011/06/27</td>
+                  <td>$183,000</td>
+                  <td>m.bruce@datatables.net</td>
+                </tr>
+                <tr>
+                  <td>Donna</td>
+                  <td>Snider</td>
+                  <td>Customer Support</td>
+                  <td>2011/01/25</td>
+                  <td>$112,000</td>
+                  <td>d.snider@datatables.net</td>
+                </tr>
+              </tbody>
+            </table>
+          </div><!-- table-wrapper -->
+        </div><!-- card -->
+
+
+
+
+      </div><!-- sl-pagebody -->
+      @include('footer')
+    </div><!-- sl-mainpanel -->
+@endsection
